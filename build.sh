c355529502c7711d892b4c38d089536d4cd4e689
Amadeus Mader <amadeus@mozart409.com>
2026-04-21 18:00:05 +0200

    feat: add tinysearch to build

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@ set -euo pipefail
 
 main() {
   ZOLA_VERSION=0.22.1
+  TINYSEARCH_VERSION=0.10.0
 
   curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
   tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+
+  curl -sLJO "https://github.com/tinysearch/tinysearch/releases/download/v${TINYSEARCH_VERSION}/tinysearch-v${TINYSEARCH_VERSION}-x86_64-unknown-linux-musl.tar.gz"
+  tar -xf tinysearch-v${TINYSEARCH_VERSION}-x86_64-unknown-linux-musl.tar.gz
 
   git submodule update --init --recursive
 
